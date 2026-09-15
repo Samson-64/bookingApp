ac1c056f22e1f1dedfea00138c57c1b8a21082ab
Samson-64 <148760141+Samson-64@users.noreply.github.com>
2026-09-15 15:05:34 +0300

    gradle setting

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -5,7 +5,6 @@ allprojects {
     }
 }
 
-/*
 val newBuildDir: Directory =
     rootProject.layout.buildDirectory
         .dir("../../build")
@@ -16,7 +15,6 @@ subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
-*/
 subprojects {
     project.evaluationDependsOn(":app")
 }
