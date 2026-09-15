e03293067a31a6f5aa54bd24e4553e8595ff4833
Samson-64 <148760141+Samson-64@users.noreply.github.com>
2026-09-15 15:06:52 +0300

    auth button uncomment

--- lib/features/auth/widgets/auth_button.dart
+++ lib/features/auth/widgets/auth_button.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+/*import 'package:flutter/material.dart';
 
 class AuthButton extends StatelessWidget {
   final String text;
@@ -29,4 +29,4 @@ class AuthButton extends StatelessWidget {
       ),
     );
   }
-}
+}*/
